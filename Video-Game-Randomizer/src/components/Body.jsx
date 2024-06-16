a2755f53390e699c984cb1385407f2ea9129d4c9
ahmell <andrewmell10@gmail.com>
2024-06-16 12:53:36 -0500

    added functionality to pseudo-search for a game, and add it to a list on the right. Submit button and using Enter to submit both work. Added button to remove all games from the list

--- Video-Game-Randomizer/src/components/Body.jsx
+++ Video-Game-Randomizer/src/components/Body.jsx
@@ -1,11 +1,33 @@
-import UserInput from './UserInput';
-import UserGames from './UserGames';
+import UserInput from "./UserInput";
+import UserGames from "./UserGames";
+import { useState } from "react";
 
 export default function Body() {
-    return (
-        <>
-        <UserInput />
-        <UserGames />
-        </>
-    )
+  const [input, setInput] = useState("");
+  const [gameList, setGameList] = useState([])
+  
+  function handleInputChange(value) {
+    setInput(value);
+  }
+
+  function handleClear(){
+    setGameList([]);
+  }
+
+  function handleSubmit() {
+    console.log(input);
+    setGameList([...gameList, input])
+  }
+
+
+
+  return (
+    <main className="grid grid-cols-2 gap-2">
+      <UserInput
+        onGameSubmit={handleSubmit}
+        onInputChange={handleInputChange}
+      />
+      <UserGames gameList={gameList} handleClear={handleClear}/>
+    </main>
+  );
 }
--- Video-Game-Randomizer/src/components/UserInput.jsx
+++ Video-Game-Randomizer/src/components/UserInput.jsx
@@ -1,8 +1,23 @@
-export default function UserInput() {
-    return (
-        <>
-        <label htmlFor="game">Select your game</label>
-         <input type='text' id="game"/>
-        </>
-    )
+
+export default function UserInput({onGameSubmit, onInputChange}) {
+  
+    function handleEnterKey(e){
+        if (e.key === 'Enter'){
+            onGameSubmit()
+        }
+      }
+
+  return (
+    <section className='outline-dashed'>
+      <label htmlFor="game"></label>
+      <input
+        type="text"
+        id="game"
+        onChange={(event) => onInputChange(event.target.value)}
+        onKeyDown={handleEnterKey}
+        className='bg-slate-300 rounded-md shadow-md outline w-2/5 m-5'
+      />
+      <button onClick={onGameSubmit} className='bg-red-300 rounded-md px-4 py-2'>Submit</button>
+    </section>
+  );
 }
